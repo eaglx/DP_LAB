2c2368cff29e95b93ad6059c263153dd110deddd
eaglx <tango_delta@outlook.com>
2018-03-05 22:02:36 +0100

    Fix

--- lab01/task1.c
+++ lab01/task1.c
@@ -9,28 +9,30 @@ int main(int argc, char **argv)
 	int i, size, tid;
 	MPI_Status status;
 
-	MPI_Init(&argv, &argv);
+	MPI_Init(&argc, &argv);
 
 	printf("Checking!\n");
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &tid);
 	printf("My id is %d from %d\n", tid, size);
 
-	i = 1;
+	i = 0;
+	
 	if(tid == ROOT)
 	{
-		MPI_Send(&i, 1, MPI_INT, i, MSG_TAG, MPI_COMM_WORLD);
-		printf("## Send %d to %d\n", i, i);
-		MPI_Recv(&i, 1, MPI_INT, size - 1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		printf("## Reciv number %d from %d\n", i, status.MPI_SOURCE);
+		MPI_Send(&i, 1, MPI_INT, (tid+1), MSG_TAG, MPI_COMM_WORLD);
+		printf("# Send %d to %d\n", i, tid + 1);
+		MPI_Recv(&i, 1, MPI_INT, (size-1), MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		printf("# Reciv number %d from %d\n", i, status.MPI_SOURCE);
+		printf("# FINISH!!!!\n");
 	}
 	else
 	{
 		MPI_Recv(&i, 1, MPI_INT, tid-1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		printf("### Reciv number %d from %d\n", i, status.MPI_SOURCE);
 		i += 1;
-		i %= size;
-		MPI_Send(&i, 1, MPI_INT, i, MSG_TAG, MPI_COMM_WORLD);
-		printf("#### Send %d to %d\n", i, i);
+		MPI_Send(&i, 1, MPI_INT, ((tid+1)%size), MSG_TAG, MPI_COMM_WORLD);
+		printf("### Send %d to %d\n", i, ((tid+1)%size));
 	}
 
 	MPI_Finalize();
